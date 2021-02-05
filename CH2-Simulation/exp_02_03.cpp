9f72d4ee9fb478a7f22ef0148725773fafc53d74
kuincan <46020217+kuincan@users.noreply.github.com>
2021-02-05 22:15:53 +0800

    exp2.3 Old Bill

--- CH2-Simulation/exp_02_03.cpp
+++ CH2-Simulation/exp_02_03.cpp
@@ -1,17 +1,17 @@
 /*
 * Old Bill
-* ��λ�۸�꣨���ۣ���*ABC*����βģ��
-* ����������N���ܼۺ��м���λ���ABC
-* �������ܼ��Լ���Ӧ�Ļ𼦵���
+* 五位价格标（单价），*ABC*，首尾模糊
+* 给出火鸡总数N、总价和中间三位标价ABC
+* 求最大的总价以及对应的火鸡单价
 */
 #include <iostream>
 #include <cstdio>
 using namespace std;
 
 int main() {
-	int x = 0, y = 0, z = 0; //�ܼ۸��м���λ
-	int num = 0; //������
-	int i = 0, j = 0; //�ܼ۸����β
+	int x = 0, y = 0, z = 0; //总价格中间三位
+	int num = 0; //火鸡总数
+	int i = 0, j = 0; //总价格的首尾
 	int flag = 0;
 	int maxfront = 0, maxtail = 0;
 	while (cin >> num)
@@ -19,9 +19,9 @@ int main() {
 		cin >> x >> y >> z;
 		flag = 0;
 		for (int i = 1; i < 10; i++)
-		{ //��
+		{ //首
 			for (int j = 0; j < 10; j++)
-			{ //β				
+			{ //尾				
 				if ((i*10000+x*1000+y*100+z*10+j) % num == 0)
 				{
 					maxfront = i;
@@ -35,4 +35,5 @@ int main() {
 		if (flag == 0)
 			cout << "0" << endl;
 	}
+	return 0;
 }
