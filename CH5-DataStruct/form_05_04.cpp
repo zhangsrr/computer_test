dc28b5fb9be351fb22e20238f865ed7ab1c5e5f2
kkhappy <runningmanzzz@163.com>
2021-02-08 10:37:07 +0800

    form5.4 Zero-complexity Transposition 栈 简单 输出逆序列

---
+++ CH5-DataStruct/form_05_04.cpp
@@ -0,0 +1,37 @@
+/**
+ * Zero-complexity Transposition
+ * 给定一组整数序列
+ * 输出其逆序列
+ * INPUT:
+ * n 序列长度
+ * n integers numbers n个整数 (-1 000 000 000 000 000 <= ai <= 1 000 000 000 000 000)
+ * 
+ * OUTPUT:
+ * 逆序列
+ **/
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+    int n=0;
+    stack<int> numbers;
+    while (cin >> n)
+    {
+        int tmp=0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> tmp;
+            numbers.push(tmp);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+                cout << numbers.top();
+            else
+                cout << " " << numbers.top();
+            numbers.pop();            
+        }        
+    }
+    return 0;
+} 
